f22052163658132919c6c48a81d9585339aaa711
Hamad <hamadnabizada@gmail.com>
2024-08-16 16:51:16 -0700

    input fields are saved to state

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import { useState } from 'react'
 function App() {
   let [addingExercise, setAddingExercise] = useState(false);
   let listOfExercises = []
+  let [exerciseForm, setExerciseForm] = useState({
+    exerciseName:'Exercise Name',
+    reps:0,
+    weight:0
+  })
   
   const emptyListElem = (
     <p>No Current Exercises</p>
@@ -14,12 +19,29 @@ function App() {
     <p>Bench Press, 5x5, 135lb</p>
   )
 
+  let [title, setTitle] = useState({first: "default title"})
+  function titleChange(e){
+    console.log(`title before change: ${title.first}`)
+    setTitle({first:"changed"})
+    console.log(`title after change: ${title.first}`)
+  }
 
   function addNewExercise(){
     setAddingExercise(true)
   }
   function submitInputForm(){
     setAddingExercise(false)
+  }
+ 
+  function handleChange(e){
+    // setExerciseForm()
+    console.log('updating change')
+    setExerciseForm(prev=>(
+      {
+        ...prev,
+        [e.target.name]:e.target.value
+      }
+    ))
   }
 
   return (
@@ -30,7 +52,8 @@ function App() {
       </div>
       <div className='workout-chart-wrapper'>
       {listOfExercises.length == 0 ? emptyListElem : listOfExercisesElem}
-      {!addingExercise ? <AddWorkout handleClick={addNewExercise} /> : <WorkoutInputForm handleSubmit={submitInputForm}/>}
+      {!addingExercise ? <AddWorkout handleClick={addNewExercise} /> : 
+        <WorkoutInputForm titleChange={titleChange} title={title} handleChange={handleChange} exerciseForm={exerciseForm} handleSubmit={submitInputForm}/>}
       </div>
     </>
   )
--- src/components/WorkoutInputForm.jsx
+++ src/components/WorkoutInputForm.jsx
@@ -1,24 +1,33 @@
 import './../componentcss/WorkoutInputForm.css'
 import SubmitButton from './SubmitButton'
 
-export default function WorkoutInputForm({handleSubmit}){
+export default function WorkoutInputForm({
+    handleSubmit,
+    exerciseForm,
+    handleChange
+}){
+
+    function logInfo(){
+        console.log(exerciseForm)
+    }
 
     return(
         <>
             <div className='input-wrapper'>
                 <div className='input-label-pair'>
-                    <input className='input-styling'></input>
+                    <input onClick={(e)=>e.target.select()} name='exerciseName' onChange={handleChange} value={exerciseForm.exerciseName} className='input-styling'></input>
                     <label>Exercise Name</label>
                 </div>
                 <div className='input-label-pair'>
-                    <input className='input-styling'></input>
+                    <input onClick={(e)=>e.target.select()}  name='reps' onChange={handleChange} value={exerciseForm.reps} className='input-styling'></input>
                     <label>Reps</label>
                 </div>
                 <div className='input-label-pair'>
-                    <input className='input-styling'></input>
+                    <input onClick={(e)=>e.target.select()} name='weight' onChange={handleChange} value={exerciseForm.weight} className='input-styling'></input>
                     <label>Weight</label>
                 </div>
                 <SubmitButton handleSubmit={handleSubmit}/>
+                <button onClick={logInfo}>LOG</button>
             </div>
         </>
     )
