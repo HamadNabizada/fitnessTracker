db571917d018cb98f7fc1a2a63ef36214f2cc450
Hamad <hamadnabizada@gmail.com>
2024-09-15 11:49:37 -0700

    handled validation on form

--- src/DailyEntry.jsx
+++ src/DailyEntry.jsx
@@ -5,12 +5,17 @@ import { useAuth } from "./context/AuthContext"
 import { useEffect, useState } from "react"
 
 export default function DailyEntry(){
-    const currentDate = format(new Date(), 'do MMMM y')
+    const date = new Date()
+    const currentDate = format(date, 'do MMMM y')
     const url = 'http://localhost:3000/api/data/daily-entry'
     const [dailyData, setDailyData] = useState({
         date: currentDate,
         weight: null,
         exercises: [{ exerciseName: '', sets: '', reps: '', weight: '' }],
+    })
+    const [errorCheck, setErrorCheck] = useState({
+        weight: false,
+        exercises: [{ exerciseName: false, sets: false, reps: false, weight: false }],
     })
 
     const { currentUser } = useAuth()
@@ -99,12 +104,28 @@ export default function DailyEntry(){
                     ...prev,
                     exercises: [...prev.exercises,
                         { exerciseName: '', sets: '', reps: '', weight: '' }]
-                }
-            )
+                    }
+                )
+            })
+            setErrorCheck(prev =>{
+                return{
+                    ...prev,
+                    exercises: [...prev.exercises,
+                        { exerciseName: false, sets: false, reps: false, weight: false }]
+
+            }
         })
     }
     function removeLine(index, e){
         setDailyData(prev=>{
+            const exerciseArray = [...prev.exercises]
+            exerciseArray.splice(index,1)
+            return{
+                ...prev,
+                exercises: exerciseArray
+            }
+        })
+        setErrorCheck(prev=>{
             const exerciseArray = [...prev.exercises]
             exerciseArray.splice(index,1)
             return{
@@ -121,20 +142,87 @@ export default function DailyEntry(){
             
             return(
                 <Box key={index} sx={styles.exerciseWrapper}>
-                    <TextField value={value.exerciseName} name="exerciseName" label="Exercise" onChange={e => handleExerciseChange(e,index)}  autoComplete="false" variant="outlined"/>
-                    <TextField value={value.sets} name="sets" label="Sets" onChange={e => handleExerciseChange(e,index)} type="number" autoComplete="false" variant="outlined"/>
-                    <TextField value={value.reps} name="reps" label="Reps" onChange={e => handleExerciseChange(e,index)} type="number" autoComplete="false" variant="outlined"/>
-                    <TextField value={value.weight} name="weight" label="Weight (lbs)" onChange={e => handleExerciseChange(e,index)} type="number" autoComplete="false" variant="outlined"/>
+                    <TextField error={errorCheck.exercises[index].exerciseName} required value={value.exerciseName} name="exerciseName" label="Exercise" onChange={e => handleExerciseChange(e,index)}  autoComplete="false" variant="outlined"/>
+                    <TextField error={errorCheck.exercises[index].sets} required value={value.sets} name="sets" label="Sets" onChange={e => handleExerciseChange(e,index)} type="number" autoComplete="false" variant="outlined"/>
+                    <TextField error={errorCheck.exercises[index].reps} required value={value.reps} name="reps" label="Reps" onChange={e => handleExerciseChange(e,index)} type="number" autoComplete="false" variant="outlined"/>
+                    <TextField error={errorCheck.exercises[index].weight} required value={value.weight} name="weight" label="Weight (lbs)" onChange={e => handleExerciseChange(e,index)} type="number" autoComplete="false" variant="outlined"/>
                     <Button onClick={(e)=> removeLine(index, e)}>Remove</Button>
                 </Box>
             )
         })
         return exerciseArray
     }
+    function ValidateData(){
+        setErrorCheck(prev =>{
+            const exercisesArray = [...prev.exercises]
+            const updatedArray = exercisesArray.map(item=>{
+                return{ 
+                    exerciseName: false, sets: false, reps: false, weight: false 
+                }
+                
+            })
+            return{
+                weight: false,
+                exercises: updatedArray
+            }
+        })
+        if(!dailyData.weight){
+            setErrorCheck(prev=>{
+                return{
+                    ...prev,
+                    weight: true
+                }
+            })
+        }
+        let noErrorsInExercisesArray = true
+        const updatedExerciseArray = dailyData.exercises.map((item,index)=>{
+            let returnExercise = {
+                exerciseName: false, sets: false, reps: false, weight: false 
+            }
+            if(!item.exerciseName){
+                noErrorsInExercisesArray = false
+                returnExercise = {
+                    ...returnExercise,
+                    exerciseName: true
+                }
+            }
+            if(!item.sets){
+                noErrorsInExercisesArray = false
+                returnExercise = {
+                    ...returnExercise,
+                    sets: true
+                }
+            }
+            if(!item.reps){
+                noErrorsInExercisesArray = false
+                returnExercise = {
+                    ...returnExercise,
+                    reps: true
+                }
+            }
+            if(!item.weight){
+                noErrorsInExercisesArray = false
+                returnExercise = {
+                    ...returnExercise,
+                    weight: true
+                }
+            }
+            return returnExercise
+        })
+        setErrorCheck(prev =>{
+            return {
+                ...prev,
+                exercises: updatedExerciseArray
+            }
+        })
+        return noErrorsInExercisesArray
+    }
     
-    // function submitData1(){
-    //     console.log(dailyData)
-    // }
+    function submitData1(){
+        ValidateData()
+        console.log(dailyData)
+        console.log(errorCheck)
+    }
 
     return (
         <Container sx={styles.container}>
@@ -147,11 +235,11 @@ export default function DailyEntry(){
                 <Typography color="primary" variant="h4">Daily Entry: {currentDate}</Typography>
                 <Box sx={styles.labelAndInputWrapper}>
                     <Typography color='secondary' variant='body1' sx={styles.label} >Weight: </Typography>
-                    <TextField onChange={handleChange} name='weight' label='lbs' type="number" autoComplete="false" variant="outlined" />
+                    <TextField error={errorCheck.weight} required onChange={handleChange} name='weight' label='lbs' type="number" autoComplete="false" variant="outlined" />
                 </Box>
                 {exerciseElems}
                 <Button onClick={updateDailyDataStateWithNewExerciseObject}><Typography >+ Add New Line</Typography></Button>
-                <Button onClick={submitData} sx={styles.button} color="secondary" variant="contained">Submit</Button>
+                <Button onClick={submitData1} sx={styles.button} color="secondary" variant="contained">Submit</Button>
             </Box>
         </Container>
     )
